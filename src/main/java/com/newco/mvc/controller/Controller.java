3c94f18d766455ac3ced13c983cd6ab151d78918
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-24 19:58:11 +0100

    added panel

--- src/main/java/com/newco/mvc/controller/Controller.java
+++ src/main/java/com/newco/mvc/controller/Controller.java
@@ -1,11 +1,18 @@
 package com.newco.mvc.controller;
 
 import com.newco.mvc.gui.MainFrame;
+import com.newco.mvc.gui.MainPanel;
 
 public class Controller {
     private MainFrame mainFrame;
 
+    private MainPanel mainPanel;
+
     public Controller() {
+        mainPanel=new MainPanel();
         mainFrame = new MainFrame();
+
+        mainFrame.setContentPane(mainPanel);
+
     }
 }
---
+++ src/main/java/com/newco/mvc/gui/MainPanel.java
@@ -0,0 +1,10 @@
+package com.newco.mvc.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainPanel extends JPanel {
+    public MainPanel(){
+        setBackground(Color.RED);
+    }
+}

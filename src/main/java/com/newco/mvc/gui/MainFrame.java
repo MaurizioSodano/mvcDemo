8d2b0db72921c6fe392dfe13b541c48524d2da0d
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-24 20:02:17 +0100

    added menu

--- src/main/java/com/newco/mvc/gui/MainFrame.java
+++ src/main/java/com/newco/mvc/gui/MainFrame.java
@@ -6,8 +6,20 @@ public class MainFrame extends JFrame {
     public MainFrame(){
         super("Swing Demo");
 
+            setJMenuBar(createMenu());
         setSize(1000,800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private JMenuBar createMenu(){
+        var menuBar =new JMenuBar();
+        var fileMenu=new JMenu("File");
+        var exitMenu=new JMenu("Exit");
+
+        fileMenu.add(exitMenu);
+        menuBar.add(fileMenu);
+
+        return menuBar;
     }
 }

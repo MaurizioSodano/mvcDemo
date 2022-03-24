f7467fa8215d211ba25abaa6cdf4756b5ebfdd24
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-24 20:06:30 +0100

    added menuItem exit

--- src/main/java/com/newco/mvc/gui/MainFrame.java
+++ src/main/java/com/newco/mvc/gui/MainFrame.java
@@ -3,21 +3,23 @@ package com.newco.mvc.gui;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
-    public MainFrame(){
+    public MainFrame() {
         super("Swing Demo");
 
-            setJMenuBar(createMenu());
-        setSize(1000,800);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setJMenuBar(createMenu());
+        setSize(1000, 800);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
     }
 
-    private JMenuBar createMenu(){
-        var menuBar =new JMenuBar();
-        var fileMenu=new JMenu("File");
-        var exitMenu=new JMenu("Exit");
+    private JMenuBar createMenu() {
+        var menuBar = new JMenuBar();
+        var fileMenu = new JMenu("File");
+        var exitItem = new JMenuItem("Exit");
 
-        fileMenu.add(exitMenu);
+        exitItem.addActionListener(e -> System.exit(0));
+
+        fileMenu.add(exitItem);
         menuBar.add(fileMenu);
 
         return menuBar;

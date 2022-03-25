59337d3f5e9fe7f504e2ac40aa0ce75a2858304a
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-25 09:25:31 +0100

    added BorderLayout

--- src/main/java/com/newco/mvc/gui/MainPanel.java
+++ src/main/java/com/newco/mvc/gui/MainPanel.java
@@ -4,7 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainPanel extends JPanel {
+
     public MainPanel(){
-        setBackground(Color.RED);
+       // setBackground(Color.RED);
+
+        var formLabel=new JLabel("Add User");
+        var nameLabel=new JLabel("Name:");
+        var passLabel=new JLabel("Password:");
+
+        var nameField=new JTextField();
+        var passField=new JTextField();
+
+        var addButton=new JButton("Save");
+
+        setLayout(new BorderLayout());
+
+        add(formLabel,BorderLayout.NORTH);
+        add(nameLabel,BorderLayout.CENTER);
     }
 }
